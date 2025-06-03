e66fa661381fd9105119423bf4e7d5646cdd0981
Anatoli Ulmer <anatoli.ulmer@gmail.com>
2025-06-03 21:27:36 +0000

    add gridsearch.py for paramter grid search

---
+++ src/models/gridsearch.py
@@ -0,0 +1,70 @@
+# gridsearch.py
+
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+import joblib
+import os
+
+RANDOM_STATE = 42
+
+
+def read_data(data_path):
+    X_test = pd.read_csv(os.path.join(data_path, 'X_test_scaled.csv'))
+    X_train = pd.read_csv(os.path.join(data_path, 'X_train_scaled.csv'))
+    y_test = pd.read_csv(os.path.join(data_path, 'y_test.csv'))
+    y_train = pd.read_csv(os.path.join(data_path, 'y_train.csv'))
+
+    return X_train, X_test, y_train, y_test
+
+
+def run_grid_search(param_grid, X_train, y_train):
+    # create model
+    model = GradientBoostingRegressor(random_state=RANDOM_STATE)
+
+    # init grid search
+    grid_search = GridSearchCV(
+        estimator=model,
+        param_grid=param_grid,
+        cv=5,  # 5-fold cross-validation
+        scoring='neg_mean_squared_error',
+        n_jobs=-1,
+        verbose=1
+    )
+
+    # run grid search for best parameters
+    grid_search.fit(X_train, y_train.values.ravel())
+    
+    return grid_search
+    
+def main():
+    data_path = "data/processed"
+    
+    X_train, X_test, y_train, y_test = read_data(data_path)
+    
+    param_grid = {
+        "n_estimators": [25, 50, 100, 200],
+        "learning_rate": [0.01, 0.33, 0.1],
+        "max_depth": [3, 5],
+        "min_samples_split": [2, 5],
+        # "min_samples_leaf": [1, 3]
+    }
+    
+    grid_search = run_grid_search(param_grid, X_train, y_train)
+    
+    print("Best params:", grid_search.best_params_)
+    best_model = grid_search.best_estimator_
+
+    y_pred = best_model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    print(f"MSE: {mse:.4f}")
+    print(f"R^2: {r2:.4f}")
+    
+    # save best parameters to file
+    joblib.dump(grid_search.best_params_, 'models/best_params.pkl')
+    
+
+if __name__=="__main__":
+    main()
